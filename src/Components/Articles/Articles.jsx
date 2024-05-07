a26ec7e9d1fbd82a34c35cb2f8a1acf42806b5a6
Stephen0wen <stevejowen@gmail.com>
2024-05-07 15:54:37 +0100

    Creates article tiles

--- src/Components/Articles/Articles.jsx
+++ src/Components/Articles/Articles.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./Articles.css";
 import { getArticles } from "../../APIs";
+import ArticleTile from "../ArticleTile/ArticleTile";
 
 const Articles = ({ slug }) => {
     const [articles, setArticles] = useState([]);
@@ -13,9 +14,11 @@ const Articles = ({ slug }) => {
     }, []);
 
     return (
-        <section id="articles">
+        <section id="articles" className="flex-center">
             {articles.map((article) => {
-                return <h2>{article.title}</h2>;
+                return (
+                    <ArticleTile key={article.article_id} article={article} />
+                );
             })}
         </section>
     );
