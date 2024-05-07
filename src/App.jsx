a114f116d2caf25a69f6d4d85cc37df6f744fc10
Stephen0wen <stevejowen@gmail.com>
2024-05-07 09:14:43 +0100

    Creates component for homepage and adds router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={Home} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
