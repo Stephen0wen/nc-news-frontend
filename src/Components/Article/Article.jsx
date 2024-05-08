ea078889a94414b09ad384008398d382132279c3
Stephen0wen <stevejowen@gmail.com>
2024-05-08 10:10:20 +0100

    Makes get request for article and displays title

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import "./Article.css";
 import { useParams } from "react-router-dom";
+import { getArticle } from "../../APIs";
 
 const Article = () => {
     const { article_id, slug } = useParams();
+
+    const [article, setArticle] = useState({});
+
+    useEffect(() => {
+        getArticle(article_id).then((apiArticle) => {
+            console.log(apiArticle);
+            setArticle(apiArticle);
+        });
+    });
+
     return (
         <article>
-            <h1>Article #{article_id}</h1>
+            <h1>{article.title}</h1>
         </article>
     );
 };
