0eed356bf90e370a2994ff99804e7450bc32fe35
Stephen0wen <stevejowen@gmail.com>
2024-05-08 10:22:07 +0100

    Adds footer to article with link back to topic and placeholders for comments and vote

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./Article.css";
 import { useParams } from "react-router-dom";
 import { getArticle } from "../../APIs";
+import ArticleFooter from "../ArticleFooter/ArticleFooter";
 
 const Article = () => {
     const { article_id, slug } = useParams();
@@ -10,15 +11,17 @@ const Article = () => {
 
     useEffect(() => {
         getArticle(article_id).then((apiArticle) => {
-            console.log(apiArticle);
             setArticle(apiArticle);
         });
-    });
+    }, []);
 
     return (
-        <article>
-            <h1>{article.title}</h1>
-        </article>
+        <>
+            <article>
+                <h1>{article.title}</h1>
+            </article>
+            <ArticleFooter slug={slug} />
+        </>
     );
 };
 
