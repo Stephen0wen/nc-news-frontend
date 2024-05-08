e0699d2a41ff6eaa06a0e485a5e8e4d2e57ebbe0
Stephen0wen <stevejowen@gmail.com>
2024-05-08 13:56:06 +0100

    Positions the comments expand box

--- src/Components/ArticleFooter/ArticleFooter.jsx
+++ src/Components/ArticleFooter/ArticleFooter.jsx
@@ -1,12 +1,18 @@
 import "./ArticleFooter.css";
 import { Link } from "react-router-dom";
+import Expand from "../Expand/Expand";
+
 const ArticleFooter = ({ slug }) => {
     return (
-        <footer id="article-footer" className="flex-center">
+        <footer id="article-footer">
             <Link to={`/${slug}`}>
                 <button>{slug}</button>
             </Link>
-            <button>Comments</button>
+            <div>
+                <Expand expandId="comments-container" label="comments">
+                    <section id="comments">Comments will go here</section>
+                </Expand>
+            </div>
             <button>Vote</button>
         </footer>
     );
