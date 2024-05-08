a8bd45f87f276eb43c8bdb381a999bf20762b2ca
Stephen0wen <stevejowen@gmail.com>
2024-05-08 12:02:19 +0100

    Enables hidding and revealing of children onClick and applies to 'start the conversation' button on homepage

--- src/Components/Expand/Expand.jsx
+++ src/Components/Expand/Expand.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
 import "./Expand.css";
 
-const Expand = ({ children }) => {};
+const Expand = ({ children, label }) => {
+    const [boxClasses, setBoxClasses] = useState("hidden");
+
+    const handleClick = () => {
+        if (boxClasses === "hidden") {
+            setBoxClasses("");
+        }
+        if (boxClasses !== "hidden") {
+            setBoxClasses("hidden");
+        }
+    };
+
+    return (
+        <>
+            <button onClick={handleClick}>{label}</button>
+            <div className={boxClasses}>{children}</div>
+        </>
+    );
+};
 
 export default Expand;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom";
 import Topics from "../Topics/Topics";
 import Welcome from "../Welcome/Welcome";
 import "./Home.css";
+import Expand from "../Expand/Expand";
 
 const Home = ({ user }) => {
     return (
         <main id="home">
             <Welcome user={user} />
             <Topics />
-            <button>Start the conversation...</button>
+            <Expand label="Start the conversation...">
+                <p>Things have expanded</p>
+            </Expand>
         </main>
     );
 };
