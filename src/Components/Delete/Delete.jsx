6e5cff30ce25e0e4b26e0ca7ddd4dddcdc2b2d3f
Stephen0wen <stevejowen@gmail.com>
2024-05-09 16:27:39 +0100

    Adds onClick handler which can differentiate between article and comment situations

--- src/Components/Delete/Delete.jsx
+++ src/Components/Delete/Delete.jsx
@@ -4,7 +4,20 @@ import { useParams } from "react-router-dom";
 const Delete = ({ comment_id }) => {
     const { article_id } = useParams();
 
-    return <button className="delete-button">Delete</button>;
+    const handleClick = () => {
+        if (comment_id) {
+            console.log("Delete comment");
+        }
+        if (!comment_id && article_id) {
+            console.log("Delete article");
+        }
+    };
+
+    return (
+        <button onClick={handleClick} className="delete-button">
+            Delete
+        </button>
+    );
 };
 
 export default Delete;
