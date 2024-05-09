3ace6eb669627d4a00f93584459805193ab01220
Stephen0wen <stevejowen@gmail.com>
2024-05-09 16:21:25 +0100

    When an article or a comment belongs to the user, the vote button is replaced with a delete component

--- src/Components/Delete/Delete.jsx
+++ src/Components/Delete/Delete.jsx
@@ -1,5 +1,10 @@
 import "./Delete.css";
+import { useParams } from "react-router-dom";
 
-const Delete = () => {};
+const Delete = ({ comment_id }) => {
+    const { article_id } = useParams();
+
+    return <button className="delete-button">Delete</button>;
+};
 
 export default Delete;
