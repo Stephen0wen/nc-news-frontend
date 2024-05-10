ab4e2c0c2a8027eae8792c2967670356c19f069c
Stephen0wen <stevejowen@gmail.com>
2024-05-10 07:24:30 +0100

    Creates component files and replaces placeoholder

---
+++ src/Components/SortArticles/SortArticles.jsx
@@ -0,0 +1,7 @@
+import "./SortArticles.css";
+
+const SortArticles = () => {
+    return <p>Hello from SortArticles</p>;
+};
+
+export default SortArticles;
--- src/Components/Topic/Topic.jsx
+++ src/Components/Topic/Topic.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Articles from "../Articles/Articles";
 import TopicFooter from "../TopicFooter/TopicFooter";
 import Expand from "../Expand/Expand";
+import SortArticles from "../SortArticles/SortArticles";
 
 const Topic = () => {
     const { slug } = useParams();
@@ -13,7 +14,7 @@ const Topic = () => {
         <main id="topic" className="flex-center">
             <h1 className="topic-title">{slug}</h1>
             <Expand expandId="sort" label="sort">
-                <p>Sorting options will go here...</p>
+                <SortArticles />
             </Expand>
             <Articles slug={slug} />
             <TopicFooter />
