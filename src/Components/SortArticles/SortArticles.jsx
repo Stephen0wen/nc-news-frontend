72bcdcf927f6789a5a1b13185b96509fcfeefffa
Stephen0wen <stevejowen@gmail.com>
2024-05-10 10:20:58 +0100

    Enables SortArticles component to add query parameters to the URL

--- src/Components/SortArticles/SortArticles.jsx
+++ src/Components/SortArticles/SortArticles.jsx
@@ -1,55 +1,85 @@
 import "./SortArticles.css";
+import { useSearchParams } from "react-router-dom";
 
 const SortArticles = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleClick = (event) => {
+        const newParams = new URLSearchParams(searchParams);
+        const order = event.target.form.elements.order.value;
+        const sortBy = event.target.form.elements["sort-by"].value;
+        newParams.set("sort_by", sortBy);
+        newParams.set("order", order);
+        setSearchParams(newParams);
+    };
+
     return (
-        <form id="sort-form">
-            <div>
-                <h2>Sort By:</h2>
-                <fieldset id="sort-by">
-                    <label for="sort-title">
-                        Title{" "}
-                        <input
-                            id="sort-title"
-                            type="radio"
-                            name="sort-by"
-                        ></input>
-                    </label>
-                    <label for="sort-author">
-                        Author{" "}
-                        <input
-                            id="sort-author"
-                            type="radio"
-                            name="sort-by"
-                        ></input>
-                    </label>
-                    <label for="sort-recent">
-                        Recent{" "}
-                        <input
-                            id="sort-recent"
-                            type="radio"
-                            name="sort-by"
-                        ></input>
-                    </label>
-                </fieldset>
-            </div>
-            <div>
-                <h2>Order:</h2>
-                <fieldset id="order">
-                    <label for="order-asc">
-                        Ascending{" "}
-                        <input id="order-asc" type="radio" name="order"></input>
-                    </label>
-                    <label for="order-desc">
-                        Descending{" "}
-                        <input
-                            id="order-desc"
-                            type="radio"
-                            name="order"
-                        ></input>
-                    </label>
-                </fieldset>
-            </div>
-        </form>
+        <>
+            <form id="sort-form">
+                <div>
+                    <div>
+                        <h2>Sort By:</h2>
+                        <fieldset id="sort-by">
+                            <label htmlFor="sort-recent">
+                                Recent{" "}
+                                <input
+                                    id="sort-recent"
+                                    type="radio"
+                                    name="sort-by"
+                                    value="recent"
+                                    defaultChecked
+                                ></input>
+                            </label>
+                            <label htmlFor="sort-title">
+                                Title{" "}
+                                <input
+                                    id="sort-title"
+                                    type="radio"
+                                    name="sort-by"
+                                    value="title"
+                                ></input>
+                            </label>
+                            <label htmlFor="sort-author">
+                                Author{" "}
+                                <input
+                                    id="sort-author"
+                                    type="radio"
+                                    name="sort-by"
+                                    value="author"
+                                ></input>
+                            </label>
+                        </fieldset>
+                    </div>
+                    <div>
+                        <h2>Order:</h2>
+                        <fieldset id="order">
+                            <label htmlFor="order-asc">
+                                Ascending{" "}
+                                <input
+                                    id="order-asc"
+                                    type="radio"
+                                    name="order"
+                                    value="asc"
+                                    defaultChecked
+                                ></input>
+                            </label>
+                            <label htmlFor="order-desc">
+                                Descending{" "}
+                                <input
+                                    id="order-desc"
+                                    type="radio"
+                                    name="order"
+                                    value="desc"
+                                ></input>
+                            </label>
+                        </fieldset>
+                    </div>
+                </div>
+                <button type="button" onClick={handleClick}>
+                    Update
+                </button>
+            </form>
+        </>
     );
 };
 
