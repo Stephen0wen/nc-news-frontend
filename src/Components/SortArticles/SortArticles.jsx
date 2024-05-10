8b01bb2324a2b66e41c4a2f7b84f549df453d34f
Stephen0wen <stevejowen@gmail.com>
2024-05-10 08:38:09 +0100

    Creates and styles form

--- src/Components/SortArticles/SortArticles.jsx
+++ src/Components/SortArticles/SortArticles.jsx
@@ -1,7 +1,56 @@
 import "./SortArticles.css";
 
 const SortArticles = () => {
-    return <p>Hello from SortArticles</p>;
+    return (
+        <form id="sort-form">
+            <div>
+                <h2>Sort By:</h2>
+                <fieldset id="sort-by">
+                    <label for="sort-title">
+                        Title{" "}
+                        <input
+                            id="sort-title"
+                            type="radio"
+                            name="sort-by"
+                        ></input>
+                    </label>
+                    <label for="sort-author">
+                        Author{" "}
+                        <input
+                            id="sort-author"
+                            type="radio"
+                            name="sort-by"
+                        ></input>
+                    </label>
+                    <label for="sort-recent">
+                        Recent{" "}
+                        <input
+                            id="sort-recent"
+                            type="radio"
+                            name="sort-by"
+                        ></input>
+                    </label>
+                </fieldset>
+            </div>
+            <div>
+                <h2>Order:</h2>
+                <fieldset id="order">
+                    <label for="order-asc">
+                        Ascending{" "}
+                        <input id="order-asc" type="radio" name="order"></input>
+                    </label>
+                    <label for="order-desc">
+                        Descending{" "}
+                        <input
+                            id="order-desc"
+                            type="radio"
+                            name="order"
+                        ></input>
+                    </label>
+                </fieldset>
+            </div>
+        </form>
+    );
 };
 
 export default SortArticles;
