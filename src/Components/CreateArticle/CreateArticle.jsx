6db4992c20ba28f5f40c1d8ccc5a78d786211e43
Stephen0wen <stevejowen@gmail.com>
2024-06-05 00:09:35 +0100

    Adds dynamic validation to the create article form

--- src/Components/CreateArticle/CreateArticle.jsx
+++ src/Components/CreateArticle/CreateArticle.jsx
@@ -1,31 +1,78 @@
 import { useParams, Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { UserContext } from "../../Contexts/UserContext";
 import "./CreateArticle.css";
 
 const CreateArticle = () => {
     const { slug } = useParams();
 
-    const handleSubmit = () => {};
+    const { user } = useContext(UserContext);
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [imgUrl, setImgUrl] = useState("");
+    const [warnings, setWarnings] = useState({
+        title: "",
+        body: "",
+        imgUrl: "",
+    });
+
+    const updateTitle = ({ target: { value } }) => {
+        setWarnings({
+            ...warnings,
+            title: value ? "" : "Please create a title.",
+        });
+    };
+
+    const updateBody = ({ target: { value } }) => {
+        setWarnings({
+            ...warnings,
+            body: value ? "" : "Please write an article body.",
+        });
+    };
+
+    const updateImgUrl = ({ target: { value } }) => {
+        setWarnings({
+            ...warnings,
+            imgUrl: value ? "" : "Please select an image.",
+        });
+    };
 
     return (
         <section id="new-article" className="flex-center">
             <h2>{slug} - Create a New Article</h2>
             <form>
                 <label>
-                    <p>Article Title</p>
-                    <input id="new-article-title" />
+                    <h3>Article Title</h3>
+                    <input
+                        id="new-article-title"
+                        onChange={updateTitle}
+                        onBlur={updateTitle}
+                    />
+                    <p>{warnings.title}</p>
                 </label>
                 <label>
-                    <p>Article Body</p>
-                    <textarea id="new-article-body" />
+                    <h3>Article Body</h3>
+                    <textarea
+                        id="new-article-body"
+                        onChange={updateBody}
+                        onBlur={updateBody}
+                    />
+                    <p>{warnings.body}</p>
                 </label>
                 <label id="-new-article-image-label">
-                    <p>Select Image File</p>
-                    <input id="new-article-image" type="file" />
+                    <h3>Select Image File</h3>
+                    <input
+                        id="new-article-image"
+                        type="file"
+                        onChange={updateImgUrl}
+                        onBlur={updateImgUrl}
+                    />
+                    <p>{warnings.imgUrl}</p>
                 </label>
                 <div id="button-container">
                     <button type="submit">Post</button>
                     <Link to={`/${slug}`}>
-                        <button type="submit">Cancel</button>
+                        <button type="button">Cancel</button>
                     </Link>
                 </div>
             </form>
