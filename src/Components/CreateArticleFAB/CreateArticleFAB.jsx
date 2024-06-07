a972101a77be954682dc70142fec4dce6df5f08f
Stephen0wen <stevejowen@gmail.com>
2024-06-07 15:19:49 +0100

    Adds FAB functionality and conditionally renders based on login

--- src/Components/CreateArticleFAB/CreateArticleFAB.jsx
+++ src/Components/CreateArticleFAB/CreateArticleFAB.jsx
@@ -1,7 +1,29 @@
 import "./CreateArticleFAB.css";
+import { UserContext } from "../../Contexts/UserContext";
+import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const CreateArticleFAB = () => {
-    return <button id="FAB">Create Article</button>;
+    const { isLoggedIn } = useContext(UserContext);
+    const { slug } = useParams();
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        if (isLoggedIn) {
+            navigate(`/create/article/${slug}`);
+        }
+    };
+
+    if (!isLoggedIn) {
+        return;
+    }
+
+    return (
+        <button id="FAB" onClick={handleClick}>
+            Create Article
+        </button>
+    );
 };
 
 export default CreateArticleFAB;
